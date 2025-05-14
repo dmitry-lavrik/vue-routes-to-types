fee23caed2ebee0b3d7b914f510e32b491fc5741
Dmitry Lavrik <admin@dmitrylavrik.ru>
2025-05-14 17:15:47 +0300

    append utility helpers and test it

---
+++ lib/utility.ts
@@ -0,0 +1,23 @@
+type PlainObj = Record<string,unknown>
+
+export type KeysAndChidlren<Obj extends PlainObj, K = keyof Obj> = 
+  K extends keyof Obj
+    ? K extends string
+      ? Obj[K] extends { children: PlainObj }
+        ? Obj[K] extends { children: never }
+          ? K
+          : K | `${K}.children.${KeysAndChidlren<Obj[K]['children']>}`
+        : K
+      : never
+    : never
+
+export type GetByDotKey<T extends PlainObj, K extends string> = 
+  K extends `${infer R1}.${infer R2}`
+    ? R1 extends keyof T
+      ? T[R1] extends PlainObj
+        ? GetByDotKey<T[R1], R2>
+        : never
+      : never
+    : K extends keyof T
+      ? T[K]
+      : never
--- tests/draft.ts
+++ tests/draft.ts
@@ -1,73 +1,109 @@
 import { GenerateRoutesDraft } from "../lib/draft";
 import { NamedRouteRecordRaw } from "../lib/types";
+import { GetByDotKey, KeysAndChidlren } from "../lib/utility";
 
 {
-const component = () => {} // always fake component
+  const component = () => {} // always fake component
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component
-  },
-  {
-    path: '/item/:id',
-    name: 'item',
-    component,
-    children: [
-      {
-        path: '',
-        name: 'item-default',
-        component
-      },
-      {
-        path: 'theme/:some',
-        name: 'item-some',
-        component
+  const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component
+    },
+    {
+      path: '/item/:id',
+      name: 'item',
+      component,
+      children: [
+        {
+          path: '',
+          name: 'item-default',
+          component
+        },
+        {
+          path: 'theme/:some',
+          name: 'item-some',
+          component,
+          children: [
+            {
+              path: 'and/:onemore(\\d)+',
+              name: 'item-some-more',
+              component
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: '/long/:parts(\\d)+',
+      name: 'long',
+      component
+    }
+  ] as const satisfies NamedRouteRecordRaw[];
+
+  type TRoutes = typeof routes;
+
+  /* Test draft */
+  const _: GenerateRoutesDraft<TRoutes> = {
+    home: {
+      name: 'home',
+      path: '/',
+      params: {},
+      children: null
+    },
+    item: {
+      name: 'item',
+      path: '/item/:id',
+      params: { id: '-' },
+      children: {
+        'item-default': {
+          name: 'item-default',
+          path: '/item/:id',
+          params: { id: '-' },
+          children: null
+        },
+        'item-some': {
+          name: 'item-some',
+          path: '/item/:id/theme/:some',
+          params: { id: '-', some: '23' },
+          children: {
+            'item-some-more': {
+              name: 'item-some-more',
+              path: '/item/:id/theme/:some/and/:onemore(\\d)+',
+              params: { id: '-', some: '23', onemore: [ ] },
+              children: null
+            }
+          }
+        }
       }
-    ]
-  },
-  {
-    path: '/long/:parts(\\d)+',
-    name: 'long',
-    component
+    },
+    long: {
+      name: 'long',
+      path: '/long/:parts(\\d)+',
+      params: { parts: [1, 2] },
+      children: null
+    }
   }
-] as const satisfies NamedRouteRecordRaw[];
 
-type TRoutes = typeof routes;
+  /* Test flatten keys */
+  const __: KeysAndChidlren<typeof _>[] = [
+    'home',
+    'item',
+    'item.children.item-default',
+    'item.children.item-some',
+    'item.children.item-some.children.item-some-more',
+    'long'
+  ];
 
-const _: GenerateRoutesDraft<TRoutes> = {
-  home: {
-    name: 'home',
-    path: '/',
-    params: {},
-    children: null
-  },
-  item: {
-    name: 'item',
+  /* Test get by dot key */
+  const ___: GetByDotKey<
+    typeof _,
+    'item.children.item-default'
+  > = {
+    name: 'item-default',
     path: '/item/:id',
     params: { id: '-' },
-    children: {
-      'item-default': {
-        name: 'item-default',
-        path: '/item/:id',
-        params: { id: '-' },
-        children: null
-      },
-      'item-some': {
-        name: 'item-some',
-        path: '/item/:id/theme/:some',
-        params: { id: '-', some: '23' },
-        children: null
-      }
-    }
-  },
-  long: {
-    name: 'long',
-    path: '/long/:parts(\\d)+',
-    params: { parts: [1, 2] },
     children: null
   }
-}
-
 }
