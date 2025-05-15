588c53f8db1ed99526eccc2e5f195e60792bd046
Dmitry Lavrik <admin@dmitrylavrik.ru>
2025-05-15 15:10:31 +0300

    better types for props & redirect

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,4 @@
-import { RouteRecordMultipleViews, RouteRecordMultipleViewsWithChildren, RouteRecordRedirect, RouteRecordSingleView, RouteRecordSingleViewWithChildren } from "vue-router";
+import { RouteLocationAsPathGeneric, RouteLocationAsRelativeGeneric, RouteLocationGeneric, RouteLocationNormalizedGeneric, RouteRecordMultipleViews, RouteRecordMultipleViewsWithChildren, RouteRecordRedirect, RouteRecordSingleView, RouteRecordSingleViewWithChildren } from "vue-router";
 
 export type RemapedParam = {
   repeatable: boolean,
@@ -13,13 +13,20 @@ type OmitKeys = 'props' | 'children' | 'redirect';
 /*
   We need simplify Vue Router types to prevent circulary errors
 */
+type RouteRecordRedirectOptionSimplified = string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric
+
 export type NamedRouteRecordRaw = (
   Omit<RouteRecordSingleView, OmitKeys> & { redirect?: never } | 
   Omit<RouteRecordSingleViewWithChildren, OmitKeys> & { redirect?: never, children: NamedRouteRecordRaw[] } | 
   Omit<RouteRecordMultipleViews, OmitKeys> & { redirect?: never } | 
   Omit<RouteRecordMultipleViewsWithChildren, OmitKeys> & { redirect?: never, children: NamedRouteRecordRaw[] } | 
-  Omit<RouteRecordRedirect, OmitKeys> & { redirect: string } //todo: redirect as fn
+  Omit<RouteRecordRedirect, OmitKeys> & { 
+    redirect: RouteRecordRedirectOptionSimplified | 
+              ((to: RouteLocationGeneric) => RouteRecordRedirectOptionSimplified)
+              // care with RouteLocationGeneric, may be it can create circular deps
+  }
 ) & { 
   name: string,
-  props?: boolean | Record<string,boolean> //todo: check other vue variants of props 
+  props?: boolean | Record<string,boolean> | ((to: RouteLocationNormalizedGeneric) => Record<string,boolean>)
+  // care with RouteLocationNormalizedGeneric, may be it can create circular deps
 };
